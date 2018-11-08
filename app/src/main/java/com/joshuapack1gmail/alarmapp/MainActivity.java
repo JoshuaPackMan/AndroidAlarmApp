661273fd323c742ca75f7b0df0abec236f9f5d43
Joshua Pack <joshuapack1@gmail.com>
2018-11-08 07:26:02 -0500

    removed unused imports

--- app/src/main/java/com/joshuapack1gmail/alarmapp/MainActivity.java
+++ app/src/main/java/com/joshuapack1gmail/alarmapp/MainActivity.java
@@ -1,23 +1,17 @@
 package com.joshuapack1gmail.alarmapp;
 
 import android.app.AlarmManager;
-import android.app.Notification;
 import android.app.PendingIntent;
 import android.app.TimePickerDialog;
 import android.content.Context;
 import android.content.Intent;
-import android.media.MediaPlayer;
 import android.support.v4.app.DialogFragment;
-import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.format.DateFormat;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.TimePicker;
-import android.widget.Toast;
 
 import java.util.Calendar;
 
